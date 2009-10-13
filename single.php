5411b7aac76a3be958f6eb330d23921e8090b089
Chris Metcalf <chris@chrismetcalf.net>
2009-10-12 23:28:07 -0700

    Getting close. Now pulling the right blog feed, style changes going into place.

--- single.php
+++ single.php
@@ -2,21 +2,21 @@
 
 <div id="main_container">
 
-	<p class="breadcrumb"><a href="<?php echo $this->config->item('base_url')?>">Home</a> &rsaquo; <?php echo $item->get_title()?></p>
-				
-	<div id="single_container">
-		<h2><a href="<?php echo $item->get_permalink()?>"><?php echo $item->get_title()?></a></h2>
-		<small><?php echo date('F j, Y g:i a', $item->get_date()); ?></small>
-		<div><?php echo $item->get_content()?></div>
+  <p class="breadcrumb"><a href="<?php echo $this->config->item('base_url')?>">Home</a> &rsaquo; <?php echo $item->get_title()?></p>
 
-		<ul class="tag_list">
-		<li>Tags: </li>
-		<?php foreach ($item->get_tags() as $tag): ?>
-			<li><a href="<?php echo $this->config->item('base_url')?>items/tag/<?php echo $tag->slug?>"><?php echo $tag->name?></a></li>
-		<?php endforeach; ?>
-		</ul>
-		
-		<!-- Insert comment script here -->
-	</div>
+  <div id="single_container">
+    <h2><a href="<?php echo $item->get_permalink()?>"><?php echo $item->get_title()?></a></h2>
+    <small><?php echo date('F j, Y g:i a', $item->get_date()); ?></small>
+    <div><?php echo $item->get_content()?></div>
+
+    <ul class="tag_list">
+    <li>Tags: </li>
+    <?php foreach ($item->get_tags() as $tag): ?>
+      <li><a href="<?php echo $this->config->item('base_url')?>items/tag/<?php echo $tag->slug?>"><?php echo $tag->name?></a></li>
+    <?php endforeach; ?>
+    </ul>
+
+    <!-- Insert comment script here -->
+  </div>
 
 </div>
